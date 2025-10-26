16ba279e79f953aad84d48ec31f8dfc4d7395d37
Anushree Dixit <anushreedixit635@gmail.com>
2025-10-26 16:40:12 +0530

    Create Question 2.c

---
+++ Assignment 1/Question 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    for(int i = 1; i <= 4; i++) {
+        for(int j = 1; j <= i; j++) {
+            if(i % 2 == 0)
+                printf("#");
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
